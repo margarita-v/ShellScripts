6e6a98a2c7f47dfbe34ab1aadb10dd26e02c175b
Margarita <margo.himera@yandex.ru>
2017-10-19 20:11:12 +0300

    Fifth task was added

---
+++ sum.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Посчитать сумму первых N натуральных чисел, N передается параметром.
+
+USAGE="Usage: sum <N> [--help]"
+INFO="Calculate a sum of first N natural numbers."
+ERROR="Error: Illegal parameters count!"
+NUMBER_EXP="^[1-9]+([0-9])?$"
+
+print_info() {
+	echo
+	echo $USAGE
+	echo $INFO
+        echo	
+}
+
+if [[ $# == 1 ]]; then
+	if [[ $1 == "--help" ]]; then
+		print_info
+	elif ! [[ $1 =~ $NUMBER_EXP ]]; then
+		echo "Error: " $1 " is not a natural number"
+		exit 1
+	else
+		echo "Sum = " $(( ($1*($1 + 1))/2 ))
+	fi
+else
+	echo $ERROR
+	print_info
+fi
